f329963e173727dda22de03eebe847c9726ff6cd
hoeghen <carverdk@yahoo.dk>
2016-04-09 09:54:51 +0200

    feedly done needs cache

--- public/javascripts/feedly.js
+++ public/javascripts/feedly.js
@@ -1,14 +1,100 @@
 /**
  * Created by cha on 4/2/2016.
  */
+var request = require('request');
 
 
-var getProviders = function(){
-    var provider = {
-        name:"bt",
-        url:"hhhh"
+var getProviders = function(callback){
+
+    request('https://cloud.feedly.com/v3/search/feeds?query=nyheder&locale=da_DK&count=50', function(error, response, body){
+        if(error) {
+            console.log(error);
+        }else{
+            callback(parse(body))
+        }
+    });
+
+    function parse(body){
+        var providers = [];
+        var response = JSON.parse(body);
+        var results = response.results;
+        results.forEach(function(result){
+            var provider = {};
+            provider.feedId = result.feedId;
+            provider.name = result.website;
+            if(provider.name == null){
+                provider.name = result.title
+            }
+            provider.iconUrl = result.iconUrl;
+            providers.push(provider)
+        });
+        return JSON.stringify(providers)
     }
-    return JSON.stringify(provider);
+//    http.get("https://cloud.feedly.com/v3/search/feeds?query=nyheder&locale=da_DK&count=50",function(){
+
+
+
+//https://cloud.feedly.com//v3/streams/contents?streamId=feed/http://borsen.dk/services/newsfeed/rss/&count=20
+};
+
+
+function paparseEntries(provider,result) {
+    var parsed = JSON.parse(result);
+    var entries = [];
+    if(parsed.items) {
+       parsed.items.forEach(function (item) {
+            var newsItem = {};
+            newsItem.title = item.title;
+            if (item.summary)
+                newsItem.summary = item.summary.content;
+            newsItem.content = item.content;
+            if (item.visual)
+                newsItem.visual = item.visual.url;
+            newsItem.published = item.published;
+            newsItem.provider = provider.name;
+            entries.push(newsItem)
+        });
+    }
+    return JSON.stringify(entries)
 }
 
+var getProviderNews = function(provider,callback){
+    var feedid = provider.feedId;
+    request('https://cloud.feedly.com//v3/streams/contents?streamId='+feedid+'&count=20', function(error, response, result){
+        if(error) {
+            console.log(error);
+        }else{
+            var entries = paparseEntries(provider,result);
+            callback(entries)
+        }
+    });
+
+};
+
+
+var getAllNews = function(callback){
+
+
+    getProviders(function(result){
+        var allList =  [];
+        var providers = JSON.parse(result)
+        var count = providers.length;
+
+        providers.forEach(function(provider){
+            getProviderNews(provider,function(list){
+               allList.push(list);
+                count = count-1;
+                if(count == 0){
+                    callback(JSON.stringify(allList))
+                }
+            })
+        })
+
+    })
+
+};
+
+
 exports.getProviders = getProviders;
+exports.getProviderNews = getProviderNews;
+exports.getAllNews = getAllNews;
--- public/javascripts/test/feedly-test.js
+++ public/javascripts/test/feedly-test.js
@@ -1,17 +1,48 @@
 var chai = require ('chai')
+var chailike = require('chai-like')
+chai.use(chailike)
 
 
 var feedly = require('../feedly')
 describe('feedly', function() {
     describe('getProviders', function () {
         it('should return a list of providers', function (done) {
-                
-            var providers = feedly.getProviders();
 
-            chai.expect(providers).to.be.json();
+            feedly.getProviders(function(response){
+                var providers = JSON.parse(response)
+                chai.expect(providers).to.be.a('array');
+                providers.forEach(function(item){
+                    chai.expect(item).to.include.keys('feedId','name')
+                })
+                done();
+            });
 
-
-            done();
         });
     });
+    describe('getProviderNews', function () {
+        it('should return a list of news', function (done1) {
+            feedly.getProviders(function(response){
+                var providers = JSON.parse(response)
+                var provider = providers[0]
+                feedly.getProviderNews(provider,function(providerNews){
+                    var providerNewsList = JSON.parse(providerNews)
+                    chai.expect(providerNewsList).to.be.a('array');
+                    done1()
+                })
+
+            })
+        });
+    });
+
+    describe('getAllNews', function () {
+        it('should return a list of all news', function (done) {
+            feedly.getAllNews(function(response){
+                var news = JSON.parse(response)
+                chai.expect(news).to.be.a('array');
+                console.log(news)
+                done()
+            })
+        });
+    });
+
 });
